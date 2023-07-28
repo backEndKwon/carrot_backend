2b2a2c36e2678d6e099dcd78c21d4c1c9ff44688
backEndKwon <rhaehfdl0433@gmail.com>
2023-07-28 16:31:14 +0900

    feat : repo 주입

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsEntity } from './posts.entity';
+import { PostsRepository } from './posts.repository';
 @Module({
   imports: [TypeOrmModule.forFeature([PostsEntity])],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService,PostsRepository]
 })
 export class PostsModule {}
