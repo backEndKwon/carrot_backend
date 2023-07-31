e26faa5e774075cfe1bb080f5ba928e205e16cdf
backEndKwon <rhaehfdl0433@gmail.com>
2023-08-01 00:41:15 +0900

    feat : config 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,9 +4,13 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'carrots',
+  // username: 'carrots', // 민석님 로컬 db
+  // username: 'carrots',//서버 db
+  username: 'postgres',// 로컬 db
   password: '5432',
-  database: 'carrots',
+  // database: 'carrots', // 민석님 로컬 db
+  // database: 'carrots', //서버 db
+  database: 'carrot_server', //로컬 db
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
